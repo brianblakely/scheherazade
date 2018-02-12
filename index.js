dba81df2c7c2726f4752dfa2a64cc20d5d29e14b
Brian Blakely <anewpage.media@gmail.com>
2018-02-12 01:51:38 +0000

    Login form

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 var path = require('path');
 var express = require('express');
+var bodyParser = require('body-parser');
 var rpio = require('rpio');
 
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(express.static('static'));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname+'/index.html'));
+  if(!req.cookies) {
+    res.sendFile(path.join(__dirname+'/login.html'));
+  } else {
+    res.sendFile(path.join(__dirname+'/index.html'));
+  }
 });
 
-app.get('/open', function(req, res) {
+app.post('/auth', function(req, res) {
+  var pw = req.body.password;
+
+  if(pw === 'f0rg0tten.c0nquest') {
+    res.cookie('loggedIn', 'true', {expires: new Date(253402300000000)});
+    res.sendFile(path.join(__dirname+'/index.html'));
+  } else {
+    res.sendFile(path.join(__dirname+'/login.html'));
+  }
+});
+
+app.get('/start', function(req, res) {
   rpio.open(7, rpio.OUTPUT);
   rpio.write(7, rpio.LOW);
-  rpio.sleep(2);
+  res.send('started');
+});
+
+app.get('/stop', function(req, res) {
   rpio.write(7, rpio.HIGH);
-  res.send('success');
+  res.send('stopped');
 });
 
 var server = app.listen(3333, function() {
